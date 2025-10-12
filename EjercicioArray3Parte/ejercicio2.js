4080d0679fd91f78822c7e0d8a5452f479603333
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-10-13 00:18:02 +0200

    update

---
+++ EjercicioArray3Parte/ejercicio2.js
@@ -0,0 +1,12 @@
+/**Crea un array llamado valores que contenga números, algunos de los cuales son
+duplicados. Utiliza un Set para crear un nuevo array que contenga solo los
+números únicos y luego imprímelo. */
+
+let valores= [67,2,34,34,67,952];
+
+//Creamos un nuevo array SET
+const lista = new Set([67,2,34,34,67,952]);
+console.log(lista);
+
+
+
