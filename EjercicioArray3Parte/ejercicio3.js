eb932f499ecb0b192f8cbe7ca585b8b70b03ff52
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-10-13 17:07:21 +0200

    hasta ejercicio 3

---
+++ EjercicioArray3Parte/ejercicio3.js
@@ -0,0 +1,9 @@
+/**Crea un Set con algunos nombres. Luego, agrega un nuevo nombre, verifica si un
+nombre específico está en el set, y elimina un nombre. Imprime el contenido del
+Set al final.*/ 
+const nombres =new set('Juan', 'ANa','Pedro');
+
+//Agregamos un nombre
+nombres.add('Maria');
+
+console.log(nombres)
