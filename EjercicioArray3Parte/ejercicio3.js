cd3f5f102e6c9dfe57c6bbc0feed776eba15fd54
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-10-13 17:14:17 +0200

    updated

--- EjercicioArray3Parte/ejercicio3.js
+++ EjercicioArray3Parte/ejercicio3.js
@@ -1,9 +1,19 @@
 /**Crea un Set con algunos nombres. Luego, agrega un nuevo nombre, verifica si un
 nombre específico está en el set, y elimina un nombre. Imprime el contenido del
 Set al final.*/ 
-const nombres =new set('Juan', 'ANa','Pedro');
 
-//Agregamos un nombre
+
+// Crea un Set con algunos nombres
+const nombres = new Set(['Juan', 'Ana', 'Pedro']);
+
+// Agregamos un nuevo nombre
 nombres.add('Maria');
 
-console.log(nombres)
+// Verificamos si un nombre específico está en el Set
+console.log(nombres); // true
+
+// Eliminamos un nombre
+nombres.delete('Pedro');
+
+// Imprimimos el contenido final del Set
+console.log(nombres);
