100dacca99c0ca2704dddc37322d181fc1cd0e04
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-10-21 15:27:28 +0200

    actualizado

---
+++ Tema3/PracticaFunciones/ejercicio1.js
@@ -0,0 +1,19 @@
+/*Crea una aplicación web que pida al usuario palabras continuamente hasta que el popup
+se deje vacío o se cancele.*/
+
+
+let palabras= [];
+let palabra = '';
+
+do {
+    palabra = prompt("Escribe aqui lo que quieras");
+    if (palabra=='') {
+        
+    }else{
+        palabras.push(palabra);
+    }
+
+
+} while (palabra==null||palabra=='');
+
+console.log(palabras);
