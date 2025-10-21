51fb1f67d22407f5e2d476a74b3460669b1cfcb0
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-10-21 18:42:42 +0200

    actualizado

--- Tema3/PracticaFunciones/practica2.js
+++ Tema3/PracticaFunciones/practica2.js
@@ -1,2 +1,17 @@
+//Funcion ejercicio 2
+
+function ejercicio2() {
+	let palabras = [];//Declaramos un array vacio 
+	let palabra = ""; //Y una palabra vacia
 
 
+	do {
+		palabra = prompt("Escribe aqui lo que quieras");
+		if (palabra != "") { //Sí la palabra no esta vacia...
+			palabras.push(palabra);//Aqui metemos la palabra en el array
+		
+		}
+
+
+	} while (palabra !== "");
+}
