d802ac82e480969a4efda4642c63c5eec4a2e0c9
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-10-23 17:12:28 +0200

    actualizado

--- Tema3/PracticaEventos/e1.js
+++ Tema3/PracticaEventos/e1.js
@@ -1,5 +1,7 @@
-/**Haz un programa que cuando se pulse un botón “Nuevo número”, añada un elemento
+/**        EJERCICIO 1
+ * Haz un programa que cuando se pulse un botón “Nuevo número”, añada un elemento
 con un número aleatorio a una lista desordenada (elemento UL). */
+
 function pulsarBoton() {
     let numero = numeroAleatorio();
     document.getElementById("numeros").innerHTML = "Número: " + numero;
@@ -10,3 +12,25 @@ function numeroAleatorio() {
 }
 
 document.getElementById("ej1").onclick = pulsarBoton;
+
+/*---------------------------------------------------------------------------------------*/ 
+
+/*        EJERCICIO 2
+Haz un programa que mediante eventos y el uso del objeto event, te muestre en todo
+momento la posición actual del ratón en pantalla. Para mostrarlo modificaremos de forma
+dinámica un elemento XHTML (Ejemplo, un <p>) que nos muestre la posición actual del
+ratón.*/ 
+
+
+document.body.addEventListener("mousemove", (evento) =>{
+
+    document.getElementById('p1'),innerHTML= 'POSICION X' + eventposicionX , 'POSICION Y' + eventposicionY;
+
+});
+
+
+
+
+
+
+
