3eb3f7fd44918a50fdb4edc4017b29d9dc68ff6a
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-11-03 00:31:27 +0100

    actualizado

--- practica2.js
+++
@@ -1,9 +0,0 @@
-class Ordenador {
-  constructor(marca, modelo, ram, capacidad, pulgadas) {
-    this.marca = marca; //Texto
-    this.modelo = modelo; //Texto
-    this.ram = ram; //Numero
-    this.capacidad = capacidad; //Numero
-    this.pulgadas = pulgadas; //Número
-  }
-}
