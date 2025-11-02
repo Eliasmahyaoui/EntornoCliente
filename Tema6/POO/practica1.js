9629ae2f1126c4ccb81dacc5e317864e10911e3e
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-11-03 00:30:01 +0100

    actualizado2.0

--- Tema6/POO/practica1.js
+++ Tema6/POO/practica1.js
@@ -16,15 +16,15 @@ class Rectangulo {
    cambiarDimensiones(num1,num2) {
     
     this.ancho= num1;// Aqui metemos en ancho el valor del num1
-    this.largo=num2;//Aqui meteemos el largo en valor del num2
+    this.alto=num2;//Aqui meteemos el largo en valor del num2
 
   }
-  calcularArea(ancho, alto){
-    return ancho* alto;
+  calcularArea(){
+    return this.ancho* this.alto;
   }
   copia(){
 
-    rectangulo= new Rectangulo(this.ancho, this.alto);
+    return new Rectangulo(this.ancho, this.alto);
   }
 
   comparar(rectangulo2){
@@ -32,26 +32,34 @@ class Rectangulo {
     const area2= rectangulo2.calcularArea();
    
     if (area1>area2) {
-      console.log("El rectangulo actual es mayor");
+      return 'mayor';
     }else if (area1==area2) {
-      console.log("Son iguales")
+    return 'iguales';
     }else{
-      console.log("El rectangulo actual es menor");
+      return 'menor';
     }
     
   }
 
 }
-const r1= new Rectangulo(4,5);
-const r2= new Rectangulo(6,8);
-r1.comparar(r2);
+
+
+// Crear rectángulos
+const r1 = new Rectangulo(4, 5);
+const r2 = new Rectangulo(6, 8);
+
+// Calcular resultados
+const area1 = r1.calcularArea();
+const area2 = r2.calcularArea();
+const comparar = r1.comparar(r2);
+const copia = r1.copia();
 
 
 //VALIDAR LOS RESULTADOS EN EL DOM 
-const DOM = document.getElementById("resultado");
-resultado.innerHTML = `
-  <p><b>Rectángulo 1:</b> ancho = ${r1.ancho}, alto = ${r1.alto}</p>
-  <p><b>Rectángulo 2:</b> ancho = ${r2.ancho}, alto = ${r2.alto}</p>
+let DOM = document.getElementById("resultado");
+DOM.innerHTML = `
+  <p>Rectángulo 1:</b> ancho = ${r1.ancho}, alto = ${r1.alto}</p>
+  <p>Rectángulo 2:</b> ancho = ${r2.ancho}, alto = ${r2.alto}</p>
   <p>Área rect1 = ${area1}</p>
   <p>Área rect2 = ${area2}</p>
   <p>Comparación: rect1 es <b>${comparar}</b> que rect2</p>
