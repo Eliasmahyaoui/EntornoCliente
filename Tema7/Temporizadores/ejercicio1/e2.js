b32f3e9b69f64a9194533fb07a202a8be0cec58d
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-11-03 18:29:21 +0100

    actualizado2.0

--- Tema7/Temporizadores/ejercicio1/e2.js
+++ Tema7/Temporizadores/ejercicio1/e2.js
@@ -1,3 +1,52 @@
 /**Ej2: Simula una “barra de carga” en consola que vaya creciendo carácter a carácter cada
 medio segundo: */
 
+//Variables 
+let contador= 0;
+let bloque= 0;
+
+for (let i = 0; i <=10; i++) {
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let bloques = 0;
+const max = 10;
+
+const intervalo = setInterval(() => {
+  bloques++;
+  console.log('█'.repeat(bloques)); // muestra 1, 2, 3... líneas
+  if (bloques >= max) {
+    clearInterval(intervalo);
+    console.log('Carga completada');
+  }
+}, 500);*/
