d2f3448d8a2d9b35ca66dd254e65b5b14ff19f5b
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-11-05 12:37:19 +0100

    actualizado

--- Tema7/Temporizadores/ejercicio1/pruebas.js
+++
@@ -1,8 +0,0 @@
-//Pruebas
-console.log("hola Elias");
-
-setTimeout(function () {
-  alert("Este mensaje se mostrará 3 segundos mas tarde..");
-}, 3000);
-
-document.getElementById("Salida2");
---
+++ Tema7/ejercicios1/ej1.js
@@ -0,0 +1,12 @@
+
+
+console.log("Nos vamos");
+
+setTimeout(()=>{
+
+    window.location.href= "https://www.google.es"
+
+},5000);
+
+
+document.getElementById("resultado");
