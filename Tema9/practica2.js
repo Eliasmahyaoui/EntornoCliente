fae8e9d0f9e47f8d9644bf57353dec3aea4d2920
Elias <eliasmahyaoui6@hotmail.com>
2025-12-01 18:37:09 +0100

    actualizado

--- Tema9/practica2.js
+++ Tema9/practica2.js
@@ -1,30 +1,60 @@
-boton.addEventListener('click', () => {
-  const idResultado = document.getElementById('input').value;
+//ESTO ES PARA EL GET
+boton.addEventListener("click", () => {
+  const idResultado = document.getElementById("input").value;
 
   const urlmodificada = `https://dummyjson.com/products/${idResultado}`;
 
   fetch(urlmodificada)
     .then((response) => {
       if (response.ok) {
+        console.log("EStoy aquii");
         return response.json();
       } else {
         throw new Error(`HTTP Error!, status: ${response.status}`);
       }
     })
 
-
     .then((data) => {
-      document.getElementById('identificacion').textContent = data.id;
-      document.getElementById('titulo').textContent = data.title;
-      document.getElementById('precio').textContent = data.price;
+      console.log(data);
+      document.getElementById("identificacion").textContent = data.id;
+      document.getElementById("titulo").textContent = data.title;
+      document.getElementById("precio").textContent = data.price;
 
+
+      const datos = {
+        id: data.id,
+        title: data.title,
+        price: data.price,
+      };
+
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify(datos),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+        })
+
+        .then((data) => {
+        
+          document.getElementById("tituloPost").textContent = data.title;
+        })
+
+        .catch((error) => {
+          console.error("Error en la petición:", error);
+        });
     })
-
 
     .catch((error) => {
       console.error(`Error al cargar los datos:`, error);
     });
-
 });
-
-
