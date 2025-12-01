97ddb6618c52a31b2decfd7a4e20e9d68e8b984c
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-12-01 04:29:25 +0100

    actualizado

--- Tema9/practica2.js
+++ Tema9/practica2.js
@@ -1,20 +1,57 @@
-fetch('https://dummyjson.com/products/12')
-  .then((response) => {
+boton.addEventListener('click', () => {
+  const idResultado = document.getElementById('input').value;
+
+  const urlmodificada = `https://dummyjson.com/products/${idResultado}`;
+
+  fetch(urlmodificada)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`HTTP Error!, status: ${response.status}`);
+      }
+    })
+
+
+    .then((data) => {
+      document.getElementById('identificacion').textContent = data.id;
+      document.getElementById('titulo').textContent = data.title;
+      document.getElementById('precio').textContent = data.price;
+
+    })
+
+
+    .catch((error) => {
+      console.error(`Error al cargar los datos:`, error);
+    });
+
+});
+
+
+const datos = {
+  id: data.id,
+  title: data.title,
+  price: data.price
+  
+};
+
+fetch('https://httpbin.org/post ', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(datos)
+  })
+  .then(response => {
     if (response.ok) {
       return response.json();
     } else {
-      throw new Error(`HTTP Error!, status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
   })
-
-  .then((data) => {
-
-    console.log(data.price);
-
-
-
+  .then(data => {
+    console.log('Respuesta del servidor:', data);
   })
-
-  .catch((error) => {
-    console.error(`Error al cargar los datos:`, error);
+  .catch(error => {
+    console.error('Error en la petición:', error);
   });
