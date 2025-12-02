532d12d5b04fed267f52ba76bdcee8cc1c02c6bf
Elias <eliasmahyaoui6@hotmail.com>
2025-12-02 18:09:00 +0100

    Actualizado

---
+++ Tema10/EjerciciosAPI/ej1.js
@@ -0,0 +1,22 @@
+
+/*Ej1: Cambia el color del fondo del sitio web según la latitud del usuario: Norte: Azul. Sur:
+Verde. Ecuador: Amarillo.*/
+
+var body = document.querySelector('body');
+navigator.geolocation.getCurrentPosition((pos) => {
+console.log(pos.coords.latitude);
+if (pos.coords.latitude==0) {
+
+    body.style.backgroundColor= "yellow";
+} else if (pos.coords.latitude<0) {
+    body.style.backgroundColor= "green";
+
+}else if(pos.coords.latitude>0){
+    body.style.backgroundColor= "blue";
+}
+
+
+//main.textContent = "Latitud:"  + pos.coords.latitude, + "Longitud:" + pos.coords.longitude;
+});
+
+
